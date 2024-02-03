e9cf14b0daec435c8ccfb98b11235b2e00527d32
m.mikic <m.mikic@rooom.com>
2024-02-03 11:34:38 +0100

    Initial Commit

---
+++ AssesmentPlayground/AssesmentPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssesmentPlaygroundApp.swift
+//  AssesmentPlayground
+//
+//  Created by Mladen Mikic on 03.02.2024..
+//
+
+import SwiftUI
+
+@main
+struct AssesmentPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
